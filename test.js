5408bdbd05717cb0a61fb4aacf4ebd62d13f4d3e
Plamen Dragiyski <plamen@dragiyski.org>
2015-10-29 23:56:26 +0200

    Remove some comments, add testing file (for the time of writing).

---
+++ test.js
@@ -0,0 +1,20 @@
+<!-- if
+var TEST1 = new Number(1);
+var TEST2 = new Number(2);
+
+var a = [TEST1, TEST2];
+
+for(var i = 0; i <= a.length; ++i) {
+    switch(a/*[i]*/) {
+        case TEST1:
+            console.log(1);
+            break;
+        case TEST2:
+            console.log(2);
+            break;
+        default:
+            console.log('default');
+           // break;
+    }
+}
+/* test */         -->
